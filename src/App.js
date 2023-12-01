73499e95ee0670db0460c5c397cf0a887eec78f8
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-11-30 20:58:43 -0500

    added get request, changed backgroudn to black, and added error handling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import Poster from './poster';
 import movieData from './MovieData';
+import { useState, useEffect } from 'react';
 
 
 function App() {
+  const [movieData, setMovieData] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Server Error!');
+        }
+      })
+      .then((data) => {
+        setMovieData(data.movies);
+      })
+      .catch((error) => {
+        setError(error.message)
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  if(error){
+    return <div className='App'>Error: {error}</div>
+  }
+
   return (
     <div className="App">
-      {movieData.movies.map((movie, index) => (
+      {movieData.map((movie, index) => (
         <Poster key={index} title={movie.title} image={movie.poster_path} />
       ))}
     </div>
