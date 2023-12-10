cdc58c412543cc2f53dc6c1ac75048bde7c32616
Brendan Turner <kisbyturner@gmail.com>
2023-12-10 13:39:16 -0700

    Removes unused code

--- src/PosterBack.js
+++ src/PosterBack.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './PosterBack.css'
 import { Link } from 'react-router-dom';
 
--- src/poster.js
+++ src/poster.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PosterBack from './PosterBack';
 import './poster.css';
 
